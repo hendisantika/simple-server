1dbc8628e1942637772aefeed3e1a5a54a343d65
Hendi Santika <hendisantika@gmail.com>
2017-08-17 08:10:21 +0700

    #4 - Add simple controller

---
+++ src/main/java/com/hendisantika/simpleserver/SimpleController.java
@@ -0,0 +1,26 @@
+package com.hendisantika.simpleserver;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/17/17
+ * Time: 8:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+@EnableAutoConfiguration
+public class SimpleController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hendi Santika - Simple Server";
+    }
+}
